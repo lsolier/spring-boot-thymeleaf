295a9e6c5dd43f1c68512e59a18556a7d670f4a2
Luis Solier Sajami <luis.sajami@gmail.com>
2019-09-28 23:55:24 -0500

    fix checkstyle

--- src/main/java/com/lsolier/udacity/springbootthymeleaf/controller/UserController.java
+++ src/main/java/com/lsolier/udacity/springbootthymeleaf/controller/UserController.java
@@ -6,7 +6,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
 import java.util.List;
 
